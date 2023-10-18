9b93e33eff51a1463eea201e1c27342b0de2afcc
ericmelz <eric@emelz.com>
2023-10-18 16:14:08 -0700

    added artifacts app

--- mine/Fedora/Fedora/Fedora/urls.py
+++ mine/Fedora/Fedora/Fedora/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('people/', include('people.urls'))
+    path('people/', include('people.urls')),
+    path('artifacts/', include('artifacts.urls')),
 ]
