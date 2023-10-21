5899b9fe023ec0b28b809a376efe8dcce87a53ea
ericmelz <eric@emelz.com>
2023-10-20 17:38:25 -0700

    added api

---
+++ mine/Sched/Sched/timelines/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, get_object_or_404
+
+from .models import Timeline
+
+
+def home(request):
+    data = {
+        "title": "Home",
+        "timelines": Timeline.objects.all(),
+    }
+
+    return render(request, "home.html", data)
+
+
+def edit_timeline(request, timeline_id):
+    timeline = get_object_or_404(Timeline, id=timeline_id)
+
+    data = {
+        "title": f"Edit Timeline {timeline.name}",
+        "timelines": timeline
+    }
+
+    return render(request, "edit_timeline.html", data)
